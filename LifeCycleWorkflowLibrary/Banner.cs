a74473852ea98d456f5db2b271a55cd5f6363a84
Peter YSZ <ysz10000@hotmail.com>
2019-11-15 16:33:08 -0500

    New development starts.

---
+++ LifeCycleWorkflowLibrary/Banner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeCycleWorkflowLibrary
+{
+    class Banner
+    {
+        private string _banner;
+        public string GetBanner()
+        {
+            return _banner;
+        }
+
+        public Banner(string Banner)
+        {
+            _banner = Banner;
+        }
+    }
+}
