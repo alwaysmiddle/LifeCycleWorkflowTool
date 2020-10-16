554d31e1609f2605577773ed5e78cc6e8b2d8c9f
Peter YSZ <ysz10000@hotmail.com>
2020-10-16 02:10:34 -0400

    Front End changes.

---
+++ LifeCycleWorkflowFrontEnd/FileLoaderWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LifeCycleWorkflowFrontEnd
+{
+    /// <summary>
+    /// Interaction logic for FileLoaderWindow.xaml
+    /// </summary>
+    public partial class FileLoaderWindow : Window
+    {
+        public FileLoaderWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- LifeCycleWorkflowFrontEnd/MainWindow.xaml.cs
+++ LifeCycleWorkflowFrontEnd/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using LifeCycleWorkflowBackend.Settings;
+﻿using System.Windows;
+
 
 namespace LifeCycleWorkflowFrontEnd
 {
@@ -24,6 +11,15 @@ namespace LifeCycleWorkflowFrontEnd
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadFileButton.Click += LoadFileButton_Click;
+        }
+
+        private void LoadFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileLoaderWindow fileLoaderWindow = new FileLoaderWindow();
+            fileLoaderWindow.Show();
+            fileLoaderWindow.Activate();
         }
     }
 }
