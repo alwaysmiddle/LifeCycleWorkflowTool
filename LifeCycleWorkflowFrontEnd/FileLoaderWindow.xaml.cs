0f2fe45ce2d6f3b42363e0a2cb867f80d098318b
Peter YSZ <ysz10000@hotmail.com>
2020-10-17 17:25:00 -0400

    Update to front end.

--- LifeCycleWorkflowFrontEnd/FileLoaderWindow.xaml.cs
+++ LifeCycleWorkflowFrontEnd/FileLoaderWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace LifeCycleWorkflowFrontEnd
 {
@@ -22,6 +14,86 @@ namespace LifeCycleWorkflowFrontEnd
         public FileLoaderWindow()
         {
             InitializeComponent();
+
+            ButtonCloseFileLoader.Click += ButtonCloseFileLoader_Click;
+            ButtonBitReport.Click += ButtonBitReport_Click;
+            ButtonWorkflow.Click += ButtonWorkflow_Click;
+            ButtonNos.Click += ButtonNos_Click;
+            ButtonUpc.Click += ButtonUpc_Click;
         }
+
+        #region Button Clicks
+
+        private void ButtonUpc_Click(object sender, RoutedEventArgs e)
+        {
+            string filepath = PopulateTextBlock(TextBlockUpc);
+
+            if (!string.IsNullOrEmpty(filepath))
+            {
+                Properties.Settings.Default.InputFileInactiveUPC = filepath;
+            }
+            
+        }
+
+        private void ButtonNos_Click(object sender, RoutedEventArgs e)
+        {
+            string filepath = PopulateTextBlock(TextBlockNos);
+
+            if (!string.IsNullOrEmpty(filepath))
+            {
+                Properties.Settings.Default.InputFileInactiveUPC = filepath;
+            }
+        }
+
+        private void ButtonWorkflow_Click(object sender, RoutedEventArgs e)
+        {
+            string filepath = PopulateTextBlock(TextBlockWorkflow);
+
+            if (!string.IsNullOrEmpty(filepath))
+            {
+                Properties.Settings.Default.InputFileInactiveUPC = filepath;
+            }
+        }
+
+        private void ButtonBitReport_Click(object sender, RoutedEventArgs e)
+        {
+            string filepath = PopulateTextBlock(TextBlockBitReport);
+
+            if (!string.IsNullOrEmpty(filepath))
+            {
+                Properties.Settings.Default.InputFileInactiveUPC = filepath;
+            }
+        }
+
+        private void ButtonCloseFileLoader_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+
+        #region Helper Function
+        /// <summary>
+        /// Helper function for temporary form
+        /// </summary>
+        public static string PopulateTextBlock(TextBlock textblock)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Open Input Files";
+
+            openFileDialog.Filter = "Data Files (.csv, .xlsx)|*.csv;*.xlsx|All Files (.*)|*.*";
+
+            if ((bool)openFileDialog.ShowDialog())
+            {
+                FileInfo file = new FileInfo(openFileDialog.FileName);
+                textblock.Text = file.Name;
+                return file.FullName;
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
     }
 }
--- LifeCycleWorkflowFrontEnd/Properties/Settings.Designer.cs
+++ LifeCycleWorkflowFrontEnd/Properties/Settings.Designer.cs
@@ -8,22 +8,90 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LifeCycleWorkflowFrontEnd.Properties
-{
-
-
+namespace LifeCycleWorkflowFrontEnd.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string InputFileInventory {
+            get {
+                return ((string)(this["InputFileInventory"]));
+            }
+            set {
+                this["InputFileInventory"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string InputFileInactiveUPC {
+            get {
+                return ((string)(this["InputFileInactiveUPC"]));
+            }
+            set {
+                this["InputFileInactiveUPC"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string InputFileDetailsProduct {
+            get {
+                return ((string)(this["InputFileDetailsProduct"]));
+            }
+            set {
+                this["InputFileDetailsProduct"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string InputFileNotOnSite {
+            get {
+                return ((string)(this["InputFileNotOnSite"]));
+            }
+            set {
+                this["InputFileNotOnSite"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string WipTemplate {
+            get {
+                return ((string)(this["WipTemplate"]));
+            }
+            set {
+                this["WipTemplate"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string FinalTemplate {
+            get {
+                return ((string)(this["FinalTemplate"]));
+            }
+            set {
+                this["FinalTemplate"] = value;
             }
         }
     }
